c564fb2b4153098967f92edd917a47be62124adb
Nayana YL <154674791+NayanaYL@users.noreply.github.com>
2025-12-23 15:33:58 +0000

    commit

---
+++ props-drilling-context/src/components/Component1.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { AppProvider } from '../context/AppContext';
+import Component2 from './Component2';
+
+const Component1 = () => {
+  return (
+    <AppProvider>
+      <Component2 />
+    </AppProvider>
+  );
+};
+
+export default Component1;
---
+++ props-drilling-context/src/context/AppContext.js
@@ -0,0 +1,21 @@
+// src/context/AppContext.js
+import React, { createContext } from 'react';
+
+export const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const contextValues = {
+    a: "Value A",
+    b: "Value B",
+    c: "Value C",
+    d: "Value D",
+    e: "Value E",
+    f: "Value F",
+  };
+
+  return (
+    <AppContext.Provider value={contextValues}>
+      {children}
+    </AppContext.Provider>
+  );
+};
